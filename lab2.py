150c178d7aa31c81b249fb0cdc1681f9772d999e
daianepagnoncelli <d_pagnoncelli@hotmail.com>
2023-12-01 10:47:11 +0100

    labs initial

---
+++ lab2.py
@@ -0,0 +1,7 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=False
+#1 , 3.14 , 5j, True, False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
---
+++ lab4.py
@@ -0,0 +1,27 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#print(myFruitList[0]) #substitua por 1,2 e verifique
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+#tupla - lista imutavel
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0]) #substitua por 1,2 e verifique
+
+#dictionary - lista com posicoes nomeadas
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
